26f9de3860fbe2e4432070e3ff0a9c5cb61c3cfa
harshs038 <harsh.s.tech@gmail.com>
2025-04-13 07:19:56 +0530

    simple input

---
+++ inputoutput.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ int arr[4];
+ printf("Enter first element :");
+ scanf("%d", &arr[0]);
+ printf("Enter second element :");
+ scanf("%d", &arr[1]);
+ printf("Enter third element :");
+ scanf("%d", &arr[2]);
+ printf("Enter fourth element :");
+ scanf("%d", &arr[3]);
+
+  printf("Third element is : %d", arr[2]);
+    return 0;
+}
