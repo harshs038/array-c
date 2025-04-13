ae53e62a8614ae7bb5e923b1ac80e0ff9bb04294
harshs038 <harsh.s.tech@gmail.com>
2025-04-13 09:22:14 +0530

    update input sys

--- sumOfarray.c
+++ sumOfarray.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 int main(){
 
-     int arr[5] = {2,4,6,8,10};
-     int sum = 0 ;
-     for (int i = 0 ; i<5 ; i++){
+    int n ;
+    printf("Enter the size of Array :");
+    scanf("%d", &n);
+    int sum = 0 ;
+    int arr[n];
+    for(int i =0 ; i<=n-1 ; i++){
+        printf("Enter numbers :");
+        scanf("%d", &arr[i]);
+
+    }
+    
+     for (int i = 0 ; i<n ; i++){
         sum = sum + arr[i];
       
      }
