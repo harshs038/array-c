506d3a9c8d5e3b88331af210e4151a16c2514f30
harshs038 <harsh.s.tech@gmail.com>
2025-04-14 18:05:52 +0530

    array in function

---
+++ functionarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void fun(int x[]){
+    x[0] = 10;
+    x[1] = 20;
+    return ;
+}
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    printf("Before function call : %d\n", arr[0]);
+    printf("Before function call : %d\n", arr[1]);
+    fun(arr);
+    printf("After function call : %d\n", arr[0]);
+    printf("Before function call : %d\n", arr[1]);
+
+
+
+    return 0;
+}
